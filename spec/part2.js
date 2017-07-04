eb5402a790754e5a5f609dcb3e535c806b5851e6
Nauda <Nauda@MAcBooks-MacBook-Air.local>
2017-07-04 10:37:36 -0700

    Completed _.extend & _.defaults

--- spec/part2.js
+++ spec/part2.js
@@ -191,9 +191,10 @@
       });
 
       it('should override properties found on the destination', function() {
-        // Replace this line with an `expect` statement that tests
-        // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        var destination = { a: 'c'};
+        var source = { a: 'b' };
+        var extended = _.extend(destination, source);
+        expect(extended.a).to.equal('b');
       });
 
       it('should not override properties not found in the source', function() {
@@ -269,9 +270,10 @@
       });
 
       it('should copy any property whose key is not already set on the target', function() {
-        // Replace this line with an `expect` statement that tests
-        // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        var destination = {a: 5};
+        var source = { a: 1, b: 2};
+        _.defaults(destination, source);
+        expect(destination.a).to.equal(5);
       });
 
       it('should not copy a property if that key is already set on the target', function() {
--- src/underbar.js
+++ src/underbar.js
@@ -273,11 +273,43 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+
+    var key, targetObject, objectKeys;
+    var args = [...arguments];
+
+    for(var i = 1; i < args.length; i++) {
+      targetObject = args[i];
+      if(targetObject !== {}){
+        objectKeys = Object.keys(targetObject);
+        for(var j = 0; j < objectKeys.length; j++) {
+          key = objectKeys[j]
+          obj[key] = targetObject[key];
+        }
+      }
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    var key, targetObject, objectKeys;
+    var args = [...arguments];
+    for(var i = 1; i < args.length; i++) {
+      targetObject = args[i];
+      if(targetObject !== {}){
+        objectKeys = Object.keys(targetObject);
+        for(var j = 0; j < objectKeys.length; j++) {
+          key = objectKeys[j]
+          if(!obj.hasOwnProperty(key)){
+            obj[key] = targetObject[key];
+          } else {
+            //obj[key] = targetObject[key];
+          }
+        }
+      }
+    }
+    return obj;
   };
 
 
